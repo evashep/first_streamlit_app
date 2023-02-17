6880ecad2df04097a1013b0991455a7a877c8484
evashep <125656328+evashep@users.noreply.github.com>
2023-02-17 11:03:00 +0000

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Smoked Salmon and Scrambled Eggs')
+streamlit.text('Overnight Oats')
+streamlit.text('Smashed Avocado on Toast')
