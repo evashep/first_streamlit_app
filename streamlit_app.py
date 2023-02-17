95d8ee9ffd1b10e1d57c48afed3e8da24b2b6e4b
evashep <125656328+evashep@users.noreply.github.com>
2023-02-17 11:05:07 +0000

    Update streamlit_app.py
    new header for special item

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('Smoked Salmon and Scrambled Eggs 🐔')
 streamlit.text('Overnight Oats 🥣')
 streamlit.text('Smashed Avocado on Toast 🥑🍞')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
